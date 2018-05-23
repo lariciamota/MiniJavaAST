14236854aa7efc1c5b1f7a88525bcb7d2a63761e
menezesmalu <mlmv@cin.ufpe.br>
2018-05-23 17:17:56 -0300

    main + teste

--- src/br/ufpe/cin/if688/minijava/main/Main.java
+++ src/br/ufpe/cin/if688/minijava/main/Main.java
@@ -1,66 +1,30 @@
 package br.ufpe.cin.if688.minijava.main;
 
-import br.ufpe.cin.if688.minijava.ast.BooleanType;
-import br.ufpe.cin.if688.minijava.ast.ClassDeclExtends;
-import br.ufpe.cin.if688.minijava.ast.ClassDeclList;
-import br.ufpe.cin.if688.minijava.ast.ClassDeclSimple;
-import br.ufpe.cin.if688.minijava.ast.Identifier;
-import br.ufpe.cin.if688.minijava.ast.IdentifierType;
-import br.ufpe.cin.if688.minijava.ast.IntegerLiteral;
-import br.ufpe.cin.if688.minijava.ast.IntegerType;
-import br.ufpe.cin.if688.minijava.ast.MainClass;
-import br.ufpe.cin.if688.minijava.ast.MethodDeclList;
-import br.ufpe.cin.if688.minijava.ast.Print;
+import br.ufpe.cin.if688.minijava.main.AntlrLexer;
+import br.ufpe.cin.if688.minijava.main.AntlrParser;
 import br.ufpe.cin.if688.minijava.ast.Program;
-import br.ufpe.cin.if688.minijava.ast.VarDecl;
-import br.ufpe.cin.if688.minijava.ast.VarDeclList;
+import br.ufpe.cin.if688.minijava.main.MyVisitor;
 import br.ufpe.cin.if688.minijava.visitor.PrettyPrintVisitor;
+
+import java.io.IOException;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+
 
 public class Main {
 
-	public static void main(String[] args) {
-		MainClass main = new MainClass(
-				new Identifier("Teste"), 
-				new Identifier("Testando"), 
-				new Print(new IntegerLiteral(2))
-		);
+	public static void main(String[] args) throws IOException {
 		
-		VarDeclList vdl1 = new VarDeclList();
-		vdl1.addElement(new VarDecl(
-			new BooleanType(),
-			new Identifier("flag")
-		));
-		vdl1.addElement(new VarDecl(
-				new IntegerType(),
-				new Identifier("num")
-		));
+		InputStream stream = new FileInputStream("src/testes/BinarySearch.java"); 
+		ANTLRInputStream input = new ANTLRInputStream(stream);
+		AntlrLexer lexer = new AntlrLexer(input);
+		CommonTokenStream token = new CommonTokenStream(lexer);
 		
-		MethodDeclList mdl = new MethodDeclList();
-		
-		ClassDeclSimple A = new ClassDeclSimple(
-					new Identifier("A"), vdl1, mdl
-		);
-		
-		ClassDeclExtends B = new ClassDeclExtends(
-				new Identifier("B"), new Identifier("A"), 
-				new VarDeclList(), new MethodDeclList()
-		);
-		
-		VarDeclList vdl2 = new VarDeclList();
-		vdl2.addElement(new VarDecl(
-				new IdentifierType("A"),
-				new Identifier("obj")
-		));
-		ClassDeclSimple C = new ClassDeclSimple(
-				new Identifier("C"), vdl2, new MethodDeclList()
-		);
-		
-		ClassDeclList cdl = new ClassDeclList();
-		cdl.addElement(A);
-		cdl.addElement(B);
-		cdl.addElement(C);
-
-		Program p = new Program(main, cdl);
+		Program p = (Program) new MyVisitor().visit(new AntlrParser(token).goal());
 		
 		PrettyPrintVisitor ppv = new PrettyPrintVisitor();
 		ppv.visit(p);
